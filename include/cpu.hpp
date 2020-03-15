37c9916702798fd21e3b09d10ccf3ad97f4392af
Andreas Economides <antroseconomides@hotmail.com>
2020-03-15 23:06:39 +0000

    Initial CPU implementation

---
+++ include/cpu.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "instruction.hpp"
+#include "window.hpp"
+
+#include <array>
+#include <random>
+#include <stack>
+
+/*
+http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#memmap
+
++---------------+= 0xFFF (4095) End of Chip-8 RAM
+|               |
+|               |
+|               |
+|               |
+|               |
+| 0x200 to 0xFFF|
+|     Chip-8    |
+| Program / Data|
+|     Space     |
+|               |
+|               |
+|               |
++- - - - - - - -+= 0x600 (1536) Start of ETI 660 Chip-8 programs
+|               |
+|               |
+|               |
++---------------+= 0x200 (512) Start of most Chip-8 programs
+| 0x000 to 0x1FF|
+| Reserved for  |
+|  interpreter  |
++---------------+= 0x000 (0) Start of Chip-8 RAM
+*/
+
+class CPU
+{
+    std::array<uint8_t, 0x1000> Memory = {0}; // 4096 bytes of RAM
+    std::stack<uint16_t> Stack;               // Stack, up to 16 16-bit addresses
+    std::vector<Instruction> ROM;             // Program data
+
+    std::array<uint8_t, 16> V = {0};                            // 16 8-bit data registers (V0, V1, ..., VF)
+    uint8_t& VF = V.back();                                     // Flag register (equivalent to V[0xF])
+    uint16_t VI = 0;                                            // 16-bit address register
+    uint16_t PC = 0;                                            // Program Counter
+    std::vector<Instruction>::const_iterator IP = ROM.cbegin(); // Instruction Pointer
+
+    Window& Display;
+
+    std::random_device Generator;                        // Random number generator
+    std::uniform_int_distribution<uint8_t> Distribution; //Random number distribution
+
+    bool UpdatePC = true;
+
+    void Decode();
+    void IncrementPC(const uint16_t Amount);
+    void SetPC(const uint16_t Address);
+
+    // Instruction set
+    void jp();
+    void jp_v0();
+    void call();
+    void ret();
+
+    void se_x_kk();
+    void se_x_y();
+    void sne_x_kk();
+    void sne_x_y();
+
+    void ld_kk() noexcept;
+    void ld_y() noexcept;
+    void ld_addr() noexcept;
+
+    void add_kk() noexcept;
+    void shr() noexcept;
+    void shl() noexcept;
+
+    void or_y() noexcept;
+    void and_y() noexcept;
+    void xor_y() noexcept;
+    void add_y() noexcept;
+    void sub_y() noexcept;
+    void subn_y() noexcept;
+
+    void rnd();
+    void drw();
+
+public:
+    CPU(Window& Display, std::vector<Instruction>&& Program);
+};
---
+++ src/cpu.cpp
@@ -0,0 +1,421 @@
+#include "../include/cpu.hpp"
+
+#include <string>
+
+constexpr std::array<uint8_t, 80> Font = {
+    0xf0, 0x90, 0x90, 0x90, 0xf0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xf0, 0x10, 0xf0, 0x80, 0xf0, // 2
+    0xf0, 0x10, 0xf0, 0x10, 0xf0, // 3
+    0x90, 0x90, 0xf0, 0x10, 0x10, // 4
+    0xf0, 0x80, 0xf0, 0x10, 0xf0, // 5
+    0xf0, 0x80, 0xf0, 0x90, 0xf0, // 6
+    0xf0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xf0, 0x90, 0xf0, 0x90, 0xf0, // 8
+    0xf0, 0x90, 0xf0, 0x10, 0xf0, // 9
+    0xf0, 0x90, 0xf0, 0x90, 0x90, // A
+    0xe0, 0x90, 0xe0, 0x90, 0xe0, // B
+    0xf0, 0x90, 0x90, 0x90, 0xf0, // C
+    0xe0, 0x90, 0x90, 0x90, 0xe0, // D
+    0xf0, 0x80, 0xf0, 0x80, 0xf0, // E
+    0xf0, 0x80, 0xf0, 0x80, 0x80  // F
+};
+
+CPU::CPU(Window& Display, std::vector<Instruction>&& ROM) : ROM(ROM), Display(Display)
+{
+    std::copy(Font.cbegin(), Font.cend(), Memory.begin());
+}
+
+void CPU::Decode()
+{
+    const auto opcode = IP->opcode();
+
+    switch (opcode)
+    {
+    case 0x00EE:
+        return ret();
+    case 0x1000:
+        return jp();
+    case 0x2000:
+        return call();
+    case 0x3000:
+        return se_x_kk();
+    case 0x4000:
+        return sne_x_kk();
+    case 0x5000:
+        return se_x_y();
+    case 0x6000:
+        return ld_kk();
+    case 0x7000:
+        return add_kk();
+    case 0x8000:
+        return ld_y();
+    case 0x8001:
+        return or_y();
+    case 0x8002:
+        return and_y();
+    case 0x8003:
+        return xor_y();
+    case 0x8004:
+        return add_y();
+    case 0x8005:
+        return sub_y();
+    case 0x8006:
+        return shr();
+    case 0x8007:
+        return subn_y();
+    case 0x800e:
+        return shl();
+    case 0x9000:
+        return sne_x_y();
+    case 0xA000:
+        return ld_addr();
+    case 0xB000:
+        return jp_v0();
+    case 0xC000:
+        return rnd();
+    case 0xD000:
+        return drw();
+    default:
+        throw std::logic_error("Opcode" + std::to_string(opcode) + "not implemented");
+        break;
+    }
+}
+
+void CPU::IncrementPC(const uint16_t Amount)
+{
+    SetPC(PC + Amount);
+}
+
+void CPU::SetPC(const uint16_t Address)
+{
+    PC = Address;
+    IP = std::next(ROM.cbegin(), PC);
+
+    if (IP >= ROM.cend())
+        throw std::out_of_range(std::to_string(PC));
+}
+
+void CPU::jp()
+{
+    /*
+    * 1nnn - JP addr
+    * Jump to location nnn.
+    *
+    * The interpreter sets the program counter to nnn.
+    */
+
+    SetPC(IP->nnn());
+    UpdatePC = false;
+}
+
+void CPU::call()
+{
+    /*
+    * 2nnn - CALL addr
+    * Call subroutine at nnn.
+    *
+    * The interpreter increments the stack pointer, then puts the current PC
+    * on the top of the stack. The PC is then set to nnn.
+    */
+
+    // The stack should only contain 16 addresses
+    if (Stack.size() >= 16)
+        throw std::runtime_error("Stack overflow");
+
+    Stack.push(PC);
+    jp();
+}
+
+void CPU::ret()
+{
+    /*
+    * 00EE - RET
+    * Return from a subroutine.
+    *
+    * The interpreter sets the program counter to the address at the top
+    * of the stack, then subtracts 1 from the stack pointer.
+    */
+
+    SetPC(Stack.top());
+    UpdatePC = false;
+
+    Stack.pop();
+}
+
+void CPU::se_x_kk()
+{
+    /*
+    * 3xkk - SE Vx, byte
+    * Skip next instruction if Vx = kk.
+    *
+    * The interpreter compares register Vx to kk, and if they are equal,
+    * increments the program counter by 2.
+    */
+
+    if (V[IP->x()] == IP->kk())
+    {
+        IncrementPC(2);
+        UpdatePC = false;
+    }
+}
+
+void CPU::sne_x_kk()
+{
+    /*
+    * 4xkk - SNE Vx, byte
+    * Skip next instruction if Vx != kk.
+    *
+    * The interpreter compares register Vx to kk, and if they are not equal,
+    * increments the program counter by 2.
+    */
+
+    if (V[IP->x()] != IP->kk())
+    {
+        IncrementPC(2);
+        UpdatePC = false;
+    }
+}
+
+void CPU::se_x_y()
+{
+    /*
+    * 5xy0 - SE Vx, Vy
+    * Skip next instruction if Vx = Vy.
+    *
+    * The interpreter compares register Vx to register Vy, and if they are
+    * equal, increments the program counter by 2.
+    */
+
+    if (V[IP->x()] == V[IP->y()])
+    {
+        IncrementPC(2);
+        UpdatePC = false;
+    }
+}
+
+void CPU::ld_kk() noexcept
+{
+    /*
+    * 6xkk - LD Vx, byte
+    * Set Vx = kk.
+    *
+    * The interpreter puts the value kk into register Vx.
+    */
+
+    V[IP->x()] = IP->kk();
+}
+
+void CPU::add_kk() noexcept
+{
+    /*
+    * 7xkk - ADD Vx, byte
+    * Set Vx = Vx + kk.
+    *
+    * Adds the value kk to the value of register Vx, then stores the result in Vx.
+    */
+
+    V[IP->x()] += IP->kk();
+}
+
+void CPU::ld_y() noexcept
+{
+    /*
+    * 8xy0 - LD Vx, Vy
+    * Set Vx = Vy.
+    *
+    * Stores the value of register Vy in register Vx.
+    */
+
+    V[IP->x()] = V[IP->y()];
+}
+
+void CPU::or_y() noexcept
+{
+    /*
+    * 8xy1 - OR Vx, Vy
+    * Set Vx = Vx OR Vy.
+    *
+    * Performs a bitwise OR on the values of Vx and Vy, then stores the
+    * result in Vx.
+    */
+
+    V[IP->x()] |= V[IP->y()];
+}
+
+void CPU::and_y() noexcept
+{
+    /*
+    * 8xy2 - AND Vx, Vy
+    * Set Vx = Vx AND Vy.
+    *
+    * Performs a bitwise AND on the values of Vx and Vy, then stores the
+    * result in Vx.
+    */
+
+    V[IP->x()] &= V[IP->y()];
+}
+
+void CPU::xor_y() noexcept
+{
+    /*
+    * 8xy3 - XOR Vx, Vy
+    * Set Vx = Vx XOR Vy.
+    *
+    * Performs a bitwise exclusive OR on the values of Vx and Vy, then
+    * stores the result in Vx.
+    */
+
+    V[IP->x()] ^= V[IP->y()];
+}
+
+void CPU::add_y() noexcept
+{
+    /*
+    * 8xy4 - ADD Vx, Vy
+    * Set Vx = Vx + Vy, set VF = carry.
+    *
+    * The values of Vx and Vy are added together. If the result is greater
+    * than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. Only the
+    * lowest 8 bits of the result are kept, and stored in Vx.
+    */
+
+    const uint_fast16_t Result = V[IP->x()] + V[IP->y()];
+    V[IP->x()] = Result & 0xFF;
+    VF = Result > 0xFF ? 1 : 0;
+}
+
+void CPU::sub_y() noexcept
+{
+    /*
+    * 8xy5 - SUB Vx, Vy
+    * Set Vx = Vx - Vy, set VF = NOT borrow.
+    *
+    * If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted
+    * from Vx, and the results stored in Vx.
+    */
+
+    V[IP->x()] -= V[IP->y()];
+    VF = V[IP->x()] > V[IP->y()] ? 1 : 0;
+}
+
+void CPU::shr() noexcept
+{
+    /*
+    * 8xy6 - SHR Vx {, Vy}
+    * Set Vx = Vx SHR 1.
+    *
+    * If the least-significant bit of Vx is 1, then VF is set to 1,
+    * otherwise 0. Then Vx is divided by 2.
+    */
+
+    VF = V[IP->x()] & 0x01;
+    V[IP->x()] >>= 2;
+}
+
+void CPU::shl() noexcept
+{
+    /*
+    * 8xyE - SHL Vx {, Vy}
+    * Set Vx = Vx SHL 1.
+    *
+    * If the most-significant bit of Vx is 1, then VF is set to 1,
+    * otherwise to 0. Then Vx is multiplied by 2.
+    */
+
+    VF = (V[IP->x()] & 0x80) >> 7;
+    V[IP->x()] <<= 2;
+}
+
+void CPU::subn_y() noexcept
+{
+    /*
+    * 8xy7 - SUBN Vx, Vy
+    * Set Vx = Vy - Vx, set VF = NOT borrow.
+    *
+    * If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted
+    * from Vy, and the results stored in Vx.
+    */
+
+    VF = V[IP->y()] > V[IP->x()] ? 1 : 0;
+    V[IP->x()] = V[IP->y()] - V[IP->x()];
+}
+
+void CPU::sne_x_y()
+{
+    /*
+    * 9xy0 - SNE Vx, Vy
+    * Skip next instruction if Vx != Vy.
+    *
+    * The values of Vx and Vy are compared, and if they are not equal, the
+    * program counter is increased by 2.
+    */
+
+    if (V[IP->x()] != V[IP->y()])
+    {
+        IncrementPC(2);
+        UpdatePC = false;
+    }
+}
+
+void CPU::ld_addr() noexcept
+{
+    /*
+    * Annn - LD I, addr
+    * Set I = nnn.
+    *
+    * The value of register I is set to nnn.
+    */
+
+    VI = IP->nnn();
+}
+
+void CPU::jp_v0()
+{
+    /*
+    * Bnnn - JP V0, addr
+    * Jump to location nnn + V0.
+    *
+    * The program counter is set to nnn plus the value of V0.
+    */
+
+    IncrementPC(V[0]);
+}
+
+void CPU::rnd()
+{
+    /*
+    * Cxkk - RND Vx, byte
+    * Set Vx = random byte AND kk.
+    *
+    * The interpreter generates a random number from 0 to 255, which is then
+    * ANDed with the value kk. The results are stored in Vx. See instruction
+    * 8xy2 for more information on AND.
+    */
+
+    V[IP->x()] = Distribution(Generator) & IP->kk();
+}
+
+void CPU::drw()
+{
+    /*
+    * Dxyn - DRW Vx, Vy, nibble
+    * Display n-byte sprite starting at memory location I at (Vx, Vy),
+    * setVF = collision.
+    *
+    * The interpreter reads n bytes from memory, starting at the address
+    * stored in I. These bytes are then displayed as sprites on screen at
+    * coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If
+    * this causes any pixels to be erased, VF is set to 1, otherwise it is
+    * set to 0. If the sprite is positioned so part of it is outside the
+    * coordinates of the display, it wraps around to the opposite side of the
+    * screen.
+    */
+
+    std::vector<uint8_t> Sprite;
+
+    for (int i = 0; i < IP->n(); ++i)
+        Sprite.push_back(Memory[VI + i]);
+
+    VF = Display.DrawSprite(Sprite, IP->x(), IP->y());
+}
