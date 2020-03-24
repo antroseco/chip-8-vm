775b15d3671f8fa461c101e398cfbe60235b06e6
Andreas Economides <antroseconomides@hotmail.com>
2020-03-24 23:40:25 +0200

    Better file handling

--- src/rom.cpp
+++ src/rom.cpp
@@ -5,27 +5,13 @@
 
 std::vector<uint8_t> LoadFile(const std::string& Path)
 {
-    /*std::ifstream File(Path, std::ios::in | std::ios::binary);
+    std::ifstream File(Path, std::ios::in | std::ios::binary);
 
     if (!File)
         throw std::runtime_error("Unable to open " + Path);
 
     const std::istreambuf_iterator<char> Iterator(File);
-    return std::vector<uint8_t>(Iterator, {});*/
-
-    std::ifstream inFile(Path, std::ios_base::binary);
-
-    inFile.seekg(0, std::ios_base::end);
-    size_t length = inFile.tellg();
-    inFile.seekg(0, std::ios_base::beg);
-
-    std::vector<uint8_t> buffer;
-    buffer.reserve(length);
-    std::copy(std::istreambuf_iterator<char>(inFile),
-              std::istreambuf_iterator<char>(),
-              std::back_inserter(buffer));
-
-    return buffer;
+    return std::vector<uint8_t>(Iterator, {});
 }
 
 bool CheckROM(const std::vector<uint8_t>& ROM) noexcept
