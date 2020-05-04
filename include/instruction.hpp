070010edaa9b593d5802fc867eb98c47761ea050
Andreas Economides <antroseconomides@hotmail.com>
2020-05-04 14:18:28 +0300

    Fix Instruction constructor
    Default constructor leaves primitive types uninitialized

--- include/instruction.hpp
+++ include/instruction.hpp
@@ -22,13 +22,13 @@ struct Instruction
     * oxyn (2 operands, 1 constant)
     */
 
-    Instruction() = default;
+    std::uint16_t raw;
+
+    constexpr Instruction() : raw(0){};
     constexpr explicit Instruction(std::uint16_t instruction) : raw(instruction){};
     explicit Instruction(const std::uint8_t* address);
 
     void read(const std::uint8_t* address);
-
-    std::uint16_t raw;
 
     std::uint_fast16_t opcode() const;
     std::uint_fast8_t x() const noexcept;
