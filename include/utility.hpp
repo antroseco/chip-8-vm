fb324d3d0aa9f0d85f13abcd64160174ca8f3964
Andreas Economides <antroseconomides@hotmail.com>
2020-05-11 21:39:31 +0300

    Fix GCC build
    Clang doesn't complain for whatever reason

--- include/utility.hpp
+++ include/utility.hpp
@@ -12,7 +12,7 @@ class byte_view
 public:
     constexpr byte_view() noexcept : Data(nullptr), Size(0){};
     constexpr byte_view(const std::uint8_t* data, std::size_t size) noexcept : Data(data), Size(size){};
-    constexpr byte_view(const std::vector<std::uint8_t>& source) noexcept : Data(source.data()), Size(source.size()){};
+    byte_view(const std::vector<std::uint8_t>& source) noexcept : Data(source.data()), Size(source.size()){};
 
     [[nodiscard]] constexpr const std::uint8_t& operator[](std::size_t position) const noexcept
     {
