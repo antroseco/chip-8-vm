7b5ff8fdf5ad6fd7445e0bab32d56402e0be55b3
Andreas Economides <antroseconomides@hotmail.com>
2020-05-16 15:08:43 +0300

    Extend and generalize byte_view

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,21 +1,40 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <stdexcept>
 #include <vector>
 
-class byte_view
+template <class T>
+class data_view
 {
-    const std::uint8_t* const Data;
-    const std::size_t Size;
+public:
+    using value_type = T;
+    using size_type = std::size_t;
+    using const_reference = const value_type&;
+    using const_pointer = const value_type*;
+    using const_iterator = const_pointer;
+
+private:
+    const const_pointer Data;
+    const size_type Size;
 
 public:
-    constexpr byte_view() noexcept : Data(nullptr), Size(0){};
-    constexpr byte_view(const std::uint8_t* data, std::size_t size) noexcept : Data(data), Size(size){};
-    byte_view(const std::vector<std::uint8_t>& source) noexcept : Data(source.data()), Size(source.size()){};
+    constexpr data_view() noexcept : Data(nullptr), Size(0){};
+    constexpr data_view(const_pointer data, size_type size) noexcept : Data(data), Size(size){};
+    data_view(const std::vector<value_type>& source) noexcept : Data(source.data()), Size(source.size()){};
 
-    [[nodiscard]] constexpr const std::uint8_t& operator[](std::size_t position) const noexcept
+    [[nodiscard]] constexpr const_reference operator[](size_type position) const noexcept
     {
+        return Data[position];
+    }
+
+    [[nodiscard]] constexpr const_reference at(size_type position) const
+    {
+        if (position >= Size)
+            throw std::out_of_range("data_view<T>::at(size_type) range check");
+
         return Data[position];
     }
 
@@ -24,13 +43,37 @@ public:
         return Size == 0;
     }
 
-    [[nodiscard]] constexpr std::size_t size() const noexcept
+    [[nodiscard]] constexpr size_type size() const noexcept
     {
         return Size;
     }
 
-    [[nodiscard]] constexpr const std::uint8_t* data() const noexcept
+    [[nodiscard]] constexpr const_pointer data() const noexcept
     {
         return Data;
     }
+
+    [[nodiscard]] constexpr const_iterator cbegin() const noexcept
+    {
+        return Data;
+    }
+
+    [[nodiscard]] constexpr const_iterator cend() const noexcept
+    {
+        return Data + Size;
+    }
+
+    [[nodiscard]] constexpr const_reference front() const noexcept
+    {
+        assert(!empty());
+        return *Data;
+    }
+
+    [[nodiscard]] constexpr const_reference back() const noexcept
+    {
+        assert(!empty());
+        return *(Data + Size - 1);
+    }
 };
+
+using byte_view = data_view<std::uint8_t>;
